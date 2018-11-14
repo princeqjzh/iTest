c1a6c9f7c1e38a750b102758e315f011d92a4a8e
钱继志 <jizhi.qian@jimu.com>
2018-11-14 12:25:56 +0800

    Add demo1

---
+++ src/main/java/com/example/Demo1.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created by jizhi.qian on 2018/11/14.
+ */
+public class Demo1 {
+    public int intAdd(int a, int b){
+        int r = a + b;
+        return r;
+    }
+}
