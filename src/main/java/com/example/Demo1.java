53ee107010eb1f0d46e50420372ca751b39bc924
princeqjzh <jizhi.qian@pintec.com>
2022-08-14 14:33:53 +0800

    Update new feature

--- src/main/java/com/example/Demo1.java
+++ src/main/java/com/example/Demo1.java
@@ -2,12 +2,12 @@ package com.example;
 
 public class Demo1 {
     public int intAdd(int a, int b){
-        int r = a + b;
+        int r = a + b + 1;
         return r;
     }
 
     public int intSubtract(int a, int b){
-        int r = a - b;
+        int r = a - b + 1;
         return r;
     }
 
